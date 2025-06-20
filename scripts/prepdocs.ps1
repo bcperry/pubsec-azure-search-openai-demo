48545e6f05453c6f4cc1455d682380629c5ca851
Blaine Perry <blainecperry@gmail.com>
2025-06-20 11:29:59 -0500

    Add environment variable loading from azd environment in prepdocs script

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -6,6 +6,23 @@ if (Test-Path -Path "/usr") {
   $venvPythonPath = "./.venv/bin/python"
 }
 
+Write-Host ""
+Write-Host "Loading azd .env file from current environment"
+Write-Host ""
+
+foreach ($line in (& azd env get-values)) {
+    if ($line -match "([^=]+)=(.*)") {
+        $key = $matches[1]
+        $value = $matches[2] -replace '^"|"$'
+        Set-Item -Path "env:\$key" -Value $value
+    }
+}
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Failed to load environment variables from azd environment"
+    exit $LASTEXITCODE
+}
+
 Write-Host 'Running "prepdocs.py"'
 
 
